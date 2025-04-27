bd9201314f34d0a793a8e17970297728c14538c7
Melodystart <start99start@gmail.com>
2025-04-27 19:05:23 +0800

    revise import model

--- website/main.py
+++ website/main.py
@@ -7,6 +7,9 @@ from predict import prediction
 import csv
 from ckip_transformers.nlp import CkipWordSegmenter, CkipPosTagger, CkipNerChunker
 import os
+import gensim
+import torch
+from torch import nn
 
 def save_to_csv(path, data):
     with open(path, mode='a', newline='', encoding='utf-8-sig') as file:
@@ -23,20 +26,45 @@ class Item(BaseModel):
     title: str
     label: str
 
-# ws_driver  = CkipWordSegmenter(model="bert-base")
-# pos_driver = CkipPosTagger(model="bert-base")
 ws_driver = None
 pos_driver = None
+doc2vec_model = None
+model = None
+
+class NeuralNetwork(nn.Module):
+  def __init__(self, input_dim, output_dim):
+      super().__init__()
+      self.stack = nn.Sequential(
+        nn.Linear(input_dim, 32),  
+        nn.ReLU(),
+        nn.Dropout(0.2),
+        nn.Linear(32, 16),
+        nn.ReLU(),
+        nn.Dropout(0.2),
+        nn.Linear(16, output_dim)
+      )
+  def forward(self, x):
+      logits = self.stack(x)
+      return logits
+
 
 tokens = []
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(BASE_DIR, "../train/user-labeled-words.csv")
 
+
 @app.on_event("startup")
 async def startup_event():
-    global ws_driver, pos_driver
+    global ws_driver, pos_driver, doc2vec_model, model
     ws_driver = CkipWordSegmenter(model="bert-base")
     pos_driver = CkipPosTagger(model="bert-base")
+    doc2vec_model = gensim.models.Doc2Vec.load("doc2vec_model_4.bin")
+
+    input_dim = doc2vec_model.vector_size
+    output_dim = 9
+    model = NeuralNetwork(input_dim, output_dim).to("cpu")
+    model.load_state_dict(torch.load("model_state_dict.pth"))
+    model.eval()
 
 @app.on_event("shutdown")
 async def shutdown_event():
@@ -52,7 +80,7 @@ async def index(request: Request):
 
 @app.get("/api/model/prediction/")
 async def get_prediction(title: str):
-    sorted_result, text_tokens = prediction(title, ws_driver, pos_driver)
+    sorted_result, text_tokens = prediction(title, ws_driver, pos_driver, doc2vec_model, model)
     global tokens
     tokens= text_tokens
     return sorted_result
--- website/predict.py
+++ website/predict.py
@@ -1,22 +1,5 @@
 import torch
 from torch import nn
-import gensim
-
-class NeuralNetwork(nn.Module):
-  def __init__(self, input_dim, output_dim):
-      super().__init__()
-      self.stack = nn.Sequential(
-        nn.Linear(input_dim, 32),  
-        nn.ReLU(),
-        nn.Dropout(0.2),
-        nn.Linear(32, 16),
-        nn.ReLU(),
-        nn.Dropout(0.2),
-        nn.Linear(16, output_dim)
-      )
-  def forward(self, x):
-      logits = self.stack(x)
-      return logits
 
 boards = {
   'Baseball':0, 
@@ -30,7 +13,7 @@ boards = {
   'Tech_Job':8
 }
 
-def prediction(title, ws_driver, pos_driver):
+def prediction(title, ws_driver, pos_driver, doc2vec_model, model):
 
   # tokenizer
   text_tokens = []
@@ -44,23 +27,12 @@ def prediction(title, ws_driver, pos_driver):
       if word_pos not in filter_pos:
         text_tokens.append(word_ws)
 
-  # print(text_tokens)
-
   # classification
   device = torch.accelerator.current_accelerator().type if torch.cuda.is_available() else "cpu"
-  doc2vec_model = gensim.models.Doc2Vec.load("doc2vec_model_4.bin")
   inferred_vector = doc2vec_model.infer_vector(text_tokens)
   input_tensor = torch.from_numpy(inferred_vector).float().unsqueeze(0).to(device)
 
   index_to_board = {v: k for k, v in boards.items()}
-
-  input_dim = doc2vec_model.vector_size
-  output_dim = len(boards)
-
-  model = NeuralNetwork(input_dim, output_dim).to(device)
-  model.load_state_dict(torch.load("model_state_dict.pth"))
-  model.eval()
-
   with torch.no_grad():
     pred = model(input_tensor)
     prob = torch.softmax(pred, dim=1)   
